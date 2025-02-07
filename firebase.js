b4f90fa6b9b927b3f09722d1911cf0e1f362f849
polaris2853 <son12b1234@gmail.com>
2025-02-08 01:10:23 +0700

    fixing gitignore attempt

--- firebase.js
+++ firebase.js
@@ -2,6 +2,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { }
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
