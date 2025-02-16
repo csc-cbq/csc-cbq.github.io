a9c14d0ab9ce20fd4fe15cacb2b43a29b7fb4e5f
polaris2853 <son12b1234@gmail.com>
2025-02-16 15:57:23 +0700

    init firebase for an.html

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
 import { db } from "./firebase.js"; // Import Firestore instance
 https://console.firebase.google.com/u/0/
 console.log("Firestore is ready:", db);
-
-
-export const form = document.getElementById("playerForm");
-console.log("form is ready")
