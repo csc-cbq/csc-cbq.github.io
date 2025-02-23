0f433a351251416a5238054b0b6d7261e867cbaa
polaris2853 <son12b1234@gmail.com>
2025-02-23 15:18:15 +0700

    js base: flag-cf, register, countdown

---
+++ JS/Flag-Checking.js
@@ -0,0 +1,27 @@
+﻿// JavaScript source code
+import { db } from "./firebase.js"; // Import Firestore instance
+console.log("Firestore is ready:", db);
+
+//Adding flags to db
+import { collection, addDoc } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("FlagCf").addEventListener("submit", async (event) => {
+        event.preventDefault(); // Prevent the page from reloading
+        const playerFlag = document.getElementById("Flag").value;
+
+
+        // if
+        try {
+            const docRef = await addDoc(collection(db, "players", "Player1",), {
+                flag: playerFlag,
+                timestamp: new Date()
+            });
+
+            alert(`✅ Valid flag!`);
+            document.getElementById("FlagCf").reset(); // Clear the form
+        } catch (error) {
+            alert("❌ Invalid flag!");
+        }
+    });
+});
---
+++ JS/Register.js
@@ -0,0 +1,28 @@
+﻿import { db } from "./firebase.js"; // Import Firestore instance
+https://console.firebase.google.com/u/0/
+console.log("Firestore is ready:", db);
+
+
+//Register
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("playerForm").addEventListener("submit", async (event) => {
+        event.preventDefault(); // Stop page refresh
+
+        const playerName = document.getElementById("playerName").value;
+        const playerEmail = document.getElementById("playerEmail").value;
+
+        try {
+            const docRef = await addDoc(collection(db, "players"), {
+                name: playerName,
+                email: playerEmail,
+                timestamp: new Date()
+            });
+
+            alert(`✅ Registered! Your ID: ${docRef.id}`);
+            document.getElementById("playerForm").reset(); // Clear form
+        } catch (error) {
+            console.error("Error:", error);
+            alert("❌ Failed to register. Try again!");
+        }
+    });
+});
