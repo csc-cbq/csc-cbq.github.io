d8434c04ded735f59a4649d592aaf0d370b195a1
polaris2853 <son12b1234@gmail.com>
2025-03-25 05:23:49 +0700

    for interview

---
+++ JS/Test.js
@@ -0,0 +1,25 @@
+import { db } from "./firebase.js"; // Import Firestore instance
+https://console.firebase.google.com/u/0/
+console.log("Firestore is ready:", db);
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("Form").addEventListener("submit", async (event) => {
+        event.preventDefault(); // Prevent the page from reloading
+        const player = document.getElementById("name").value;
+        const answer = document.getElementById("code").value;
+
+        // if
+        if (answer == ) {
+            const docRef = await addDoc(collection(db, "players"), {
+                testee: player,
+                level_1: answer,
+                timestamp: new Date()
+            });
+
+            alert(`? Submitted!`);
+            document.getElementById("FlagCf").reset(); // Clear the form
+        } else {
+            console.log("? The code wasn't right.");
+        }
+    });
+});
