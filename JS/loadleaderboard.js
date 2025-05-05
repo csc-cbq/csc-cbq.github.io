41cff5c34f9a329d7cbf20e32e26c5887c665f40
polaris2853 <son12b1234@gmail.com>
2025-05-05 21:14:28 +0700

    Verifying Admin using document's ID per user instead of displayName

--- JS/loadleaderboard.js
+++ JS/loadleaderboard.js
@@ -1,38 +1,41 @@
 ﻿import { db, collection, getDocs, query, orderBy, limit } from "./firebase.js";
 
+// Admin's ID list
 const admin = [
-"Sơn Nguyễn",
-]
+	"jUfXLiPKZIbgQmmdmWMzFcGh5fw2",
+];
 
 async function loadLeaderboard() {
 
-    const playersRef = collection(db, "players");
-    const q = query(playersRef, orderBy("flagCount", "desc"), orderBy("timestamp", "desc")); 
-    const querySnapshot = await getDocs(q);
+	const playersRef = collection(db, "players");
+	const q = query(playersRef, orderBy("flagCount", "desc"), orderBy("timestamp", "desc")); 
+	const querySnapshot = await getDocs(q);
 
-    const tbody = document.querySelector("#leaderboard tbody");
-    tbody.innerHTML = ""; // clear table
+	const tbody = document.querySelector("#leaderboard tbody");
+	tbody.innerHTML = ""; // clear table
 
-    
-    querySnapshot.forEach((doc) => {
-        const data = doc.data();
-        let name = data.Name
-        let rank = 1;
+	// Scanning through each docs in the snapshot
+	querySnapshot.forEach((doc) => {
+		const data = doc.data();
+		const docid = doc.id;
+		let name = data.Name;
+		let rank = 1;
 
-        if (admin.includes(name)) {
-            name = `${data.Name} (Admin)`
-            rank = 0
-        }
-        const row = document.createElement("tr");
-        row.innerHTML = `
-        <th>${rank++}</th>
-        <th>${name}</th>
-        <th>${data.flagCount || 0}</th>
-        `;
-        tbody.appendChild(row);
-    });
+		// If user (document) is admin
+		if (admin.includes(docid)) {
+			name = `${data.Name} (Admin)`;
+			rank = 0;
+		}
+
+		// Adding entries into the table
+		const row = document.createElement("tr");
+		row.innerHTML = `
+		<th>${rank++}</th>
+		<th>${name}</th>
+		<th>${data.flagCount || 0}</th>
+		`;
+		tbody.appendChild(row);
+	});
 }
 
-
 document.addEventListener("DOMContentLoaded", loadLeaderboard);
-
